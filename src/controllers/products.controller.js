eb8864e55df461ea85b2e9b8e087f9a328218a5d
Ludson96 <ludson_ps27@hotmail.com>
2022-11-15 17:57:41 -0300

    add atualiza produto na camada controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -20,8 +20,17 @@ const insert = async (req, res) => {
   res.status(201).json(message);
 };
 
+const updateById = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const { type, message } = await productsService.updateById(name, id);
+  if (type) return res.status(404).json({ message: 'Product not found' });
+  return res.status(200).json(message);
+};
+
 module.exports = {
   findAll,
   findById,
   insert,
+  updateById,
 };
