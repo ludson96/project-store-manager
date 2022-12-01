80ba0d070b17b05ef0e13f7700b17b717b13b660
Ludson96 <ludson_ps27@hotmail.com>
2022-12-01 11:57:34 -0300

    atualizando validation

--- src/services/validations/sales.validation.js
+++ src/services/validations/sales.validation.js
@@ -1,7 +1,29 @@
-const validateProductId = (sale) => {
+const { salesModel } = require('../../models');
+
+const MAP = async (productId) => {
+  const allSales = await salesModel.getAllSales();
+  const teste = allSales.every((sale) => {
+    console.log(sale.productId);
+   return sale.productId === productId;
+  });
+  console.log('Eu sou o array do every: ', teste);
+};
+
+const functionAllSales = async () => {
+  if (await MAP()) {
+    return true; 
+  }
+  return false;
+};
+
+const validateProductId = (sales) => {
   let errorMessage;
-    sale.map(({ productId }) => {
-      if (productId > 99) errorMessage = { type: 'DRIVER_NOT_FOUND', message: 'Product not found' };
+  
+  sales.map(async ({ productId }) => {
+    console.log('Eu sou productId do validation: ', productId);
+    if (await functionAllSales()) {
+        errorMessage = { type: 'DRIVER_NOT_FOUND', message: 'Product not found' };
+      }
       return false;
     });
   if (errorMessage) return errorMessage;
@@ -10,4 +32,8 @@ const validateProductId = (sale) => {
 
 module.exports = {
   validateProductId,
-  };
+};
+  
+//   if (!body.every((sale) => 'productId' in sale)) {
+//     return res.status(400).json({ message: '"productId" is required' });
+//   }
